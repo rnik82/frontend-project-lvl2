440461a8cf199a6cd7ad228f297f23317ae654c4
Roman <rnik82@mail.ru>
2020-04-10 17:12:19 +0300

    update renderPlain

--- src/formatters/renderPlain.js
+++ src/formatters/renderPlain.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
 
+const makeValue = (value) => {
+    const newValue = _.isObject(value) ? '[complex value]' : value;
+    return _.isString(newValue) && newValue !== '[complex value]'
+        ? `'${newValue}'` : newValue;
+};
+
 const makeLine = (status, ancestors, value1, value2) => {
     const property = ancestors.join('.');
 
-    const newValue1 = _.isString(value1) && value1 !== '[complex value]' ? `'${value1}'` : value1;
-    const newValue2 = _.isString(value2) && value2 !== '[complex value]' ? `'${value2}'` : value2;
-
     const chooseRow = {
-        changed: `Property '${property}' was changed from ${newValue1} to ${newValue2}`,
-        added: `Property '${property}' was added with value: ${newValue1}`,
+        changed: `Property '${property}' was changed from ${value1} to ${value2}`,
+        added: `Property '${property}' was added with value: ${value1}`,
         deleted: `Property '${property}' was deleted`,
     }
     const line = chooseRow[status];
@@ -22,13 +25,10 @@ const renderPlain = (ast, ancestors = [], accR = '') => {
             return renderPlain(children, newAncestors, acc);
         }
 
-        const newValue = _.isObject(value) ? '[complex value]' : value;
-        const newValue2 = _.isObject(value2) ? '[complex value]' : value2;
-
         if (status === 'unchanged') {
             return acc;
         }
-        const newLine = makeLine(status, newAncestors, newValue, newValue2);
+        const newLine = makeLine(status, newAncestors, makeValue(value), makeValue(value2));
         return `${acc}\n${newLine}`;
     }, accR);
     return result.trim();
