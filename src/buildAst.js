67ea093877b63944ad9bfdc5af3cd34b973bba01
rnik82 <rnik82@mail.ru>
2020-05-15 15:02:53 +0300

    update after check5

--- src/buildAst.js
+++ src/buildAst.js
@@ -10,25 +10,21 @@ const buildAst = (config1, config2) => {
         status: 'added', key, value1: null, value2, children: [],
       };
     }
-
     if (!_.has(obj2, key)) {
       return {
         status: 'deleted', key, value1, value2: null, children: [],
       };
     }
-
     if (_.isObject(value1) && _.isObject(value2)) {
       return {
         status: 'nested', key, children: buildAst(value1, value2),
       };
     }
-
     if (value1 !== value2) {
       return {
         status: 'changed', key, value1, value2, children: [],
       };
     }
-
     return {
       status: 'unchanged', key, value1, children: [],
     };
